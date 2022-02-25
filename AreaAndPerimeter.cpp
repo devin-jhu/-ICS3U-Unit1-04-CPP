37c321f81436feb79eea0a8556294546f1fddfa5
Devin Jhu <devin.jhu@mths.ca>
2022-02-25 19:35:35 +0000

    added calculator

---
+++ AreaAndPerimeter.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2022 Devin Jhu All rights reserved
+//
+// Created by Devin Jhu
+// Created on February 2022
+// This calculates the area and perimeter
+// Currently calculating for 3cm by 5cm rectangle
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions: " << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
+}
